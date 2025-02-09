e2e33b18bf5b6e36dbf93bcdb0e317ae6000a555
VMK003 <vmkanavade@aissmscoe.com>
2025-02-09 20:16:49 +0530

    Update test2.py

--- test2.py
+++ test2.py
@@ -1,1 +1,11 @@
-print("hello");
+ from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return "Hello, Google Cloud!"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)  # Make it accessible
+
